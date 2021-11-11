1ad67b46b2a2cc92d5ab455d3abfed980be11083
akhmad fikron huda <akhfih94@gmail.com>
2021-11-11 16:47:42 +0700

    latihan service worker

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '4fdc131e50b225901e0115047568bf57',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
